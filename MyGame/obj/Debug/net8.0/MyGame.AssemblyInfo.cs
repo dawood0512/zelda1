798d63f8a433efb8a15909c47ff930f6a97b6f9f
dawood0512 <dawoodjee12@gmail.com>
2025-10-23 10:49:29 +0200

    Add .gitignore and untrack build artifacts

--- MyGame/obj/Debug/net8.0/MyGame.AssemblyInfo.cs
+++ MyGame/obj/Debug/net8.0/MyGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f7c9cbadc539de6c705b7eeb0566dbbf27e5a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
